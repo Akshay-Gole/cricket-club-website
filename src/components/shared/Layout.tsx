190505f6eb0e6daddf2e9638cb483095150fca15
Akshay Gole <akshaygole01111@gmail.com>
2026-06-16 15:52:03 +0930

    feat: add ErrorBoundary component and wrap page content

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import ErrorBoundary from './ErrorBoundary'
 
 function Layout() {
   return (
     <>
       <Navbar />
       <main className="pt-[72px]">
-        <Outlet />
+        <ErrorBoundary name="PageContent">
+          <Outlet />
+        </ErrorBoundary>
       </main>
       <Footer />
     </>
